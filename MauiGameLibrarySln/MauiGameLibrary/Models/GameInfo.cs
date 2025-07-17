bd0f9949793755bd02f501c9f7af5af97fa65768
n9izyboy <silvanocartinio@gmail.com>
2025-07-17 12:25:59 +0200

    Blah

---
+++ MauiGameLibrarySln/MauiGameLibrary/Models/GameInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiGameLibrary.Models
+{
+    public class GameInfo
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+
+        public string GameType { get; set; }
+
+        public int Genre { get; set; }
+
+        public string AgeRestriction { get; set; }
+
+        public bool Multiplayer { get; set; }
+
+        public string Description { get; set; }
+
+        public string Image { get; set; }
+
+        public DateTime YearPublished { get; set; }
+    }
+}
---
+++ MauiGameLibrarySln/MauiGameLibrary/Services/GameDataService.cs
@@ -0,0 +1,50 @@
+﻿using MauiGameLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiGameLibrary.Services
+{
+    public class GameDataService
+    {
+        private List<GameInfo> _gameInfo = new List<GameInfo>();
+
+        public List<GameInfo> GetAllGameInfo()
+        {
+            return _gameInfo;    
+                
+        }
+
+        public void UpdateGameInfo(GameInfo gameInfo)
+        {
+            if (!string.IsNullOrEmpty(gameInfo.Id))
+            {
+
+                var uniqueGame = GetGameInfoById(gameInfo.Id);
+
+                int position = _gameInfo.IndexOf(uniqueGame);
+
+                _gameInfo[position] = gameInfo;
+            }
+            else
+            {
+                _gameInfo.Add(gameInfo);
+            }
+
+        }
+        public GameInfo GetGameInfoById(string id)
+        {
+            var uniqueGame = _gameInfo.Where(gameInfo => gameInfo.Id == id).FirstOrDefault();
+            return uniqueGame;
+        }
+        
+
+        public List <GameInfo> GetGameInfoByTitle (string title)
+        {
+           return _gameInfo.Where(gameInfo => gameInfo.Title == title).ToList();
+
+        }
+    }
+}
